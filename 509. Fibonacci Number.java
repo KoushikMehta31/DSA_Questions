50681f901d84685c7e6a0edb8890c04288018f99
Koushik Mehta <koushikmehta07@gmail.com>
2025-07-31 23:17:20 +0530

    Create 509. Fibonacci Number.java

---
+++ 509. Fibonacci Number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        int firstterm = 0;
+        int secondterm = 1;
+
+        for(int i=1;i<=n;i++){
+            int thirdterm = firstterm + secondterm;
+
+            firstterm = secondterm;
+            secondterm = thirdterm;
+        }
+        return firstterm;
+    }
+}
